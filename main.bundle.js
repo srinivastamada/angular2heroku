ac0965916976bc1051f76e9e1ae3e8689b450ead
Srinivas Tamada <srinivas@9lessons.info>
2019-04-15 00:35:28 -0400

    New change

---
+++ main.bundle.js
@@ -0,0 +1,19 @@
+webpackJsonp([1,2],{
+
+/***/ 1:
+/***/ function(module, exports) {
+
+throw new Error("Module build failed: TypeError: Cannot read property 'directoryExists' of undefined\n    at Object.getAutomaticTypeDirectiveNames (/Users/SrinivasTamada/Projects/angular2heroku/node_modules/typescript/lib/typescript.js:43865:18)\n    at State.loadTypesFromConfig (/Users/SrinivasTamada/Projects/angular2heroku/node_modules/awesome-typescript-loader/src/host.ts:150:36)\n    at new State (/Users/SrinivasTamada/Projects/angular2heroku/node_modules/awesome-typescript-loader/src/host.ts:144:14)\n    at Object.ensureInstance (/Users/SrinivasTamada/Projects/angular2heroku/node_modules/awesome-typescript-loader/src/instance.ts:150:19)\n    at compiler (/Users/SrinivasTamada/Projects/angular2heroku/node_modules/awesome-typescript-loader/src/index.ts:37:20)\n    at Object.loader (/Users/SrinivasTamada/Projects/angular2heroku/node_modules/awesome-typescript-loader/src/index.ts:18:18)");
+
+/***/ },
+
+/***/ 7:
+/***/ function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(1);
+
+
+/***/ }
+
+},[7]);
+//# sourceMappingURL=main.map
